19d3a6fbc7049471736c43648e9a38cf9e570a14
Ali Tahvildari <a.tahvildari1997@gmail.com>
2023-03-10 11:05:15 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 27;
+        System.out.println(name); //print name
+        System.out.println(age); //print age
+        System.out.println(name + " " +age); //print HL
+        float meinNum = 15.99f;
+        double meinNum1 = 15.99d;
+        System.out.print(meinNum);
+
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+    }
+}
